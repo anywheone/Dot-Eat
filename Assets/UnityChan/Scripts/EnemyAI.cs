8c96a59d7095bbc93ff46ac51c2f3c99f6e88eca
tatsuya imaizumi <q.3.ptatsuya@gmail.com>
2023-02-22 15:30:29 +0900

    first commit

---
+++ Assets/UnityChan/Scripts/EnemyAI.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform PlayerPosition;//プレイヤーの位置
+	static Vector3 pos;
+	UnityEngine.AI.NavMeshAgent agent;
+	float agentToPatroldistance;
+	float agentToPlayerdistance;
+	float Espeed;
+	private Rigidbody Enemy;
+
+	void Awake(){
+	    agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+
+	void Start () {
+		Enemy = this.GetComponent<Rigidbody> ();
+	    //コルーチン実行(start) ←処理を実行し続ける
+	}
+
+	void FixedUpdate(){
+	    //プレイヤーの位置を取得
+	    PlayerPosition = GameObject.FindWithTag("Player").transform;
+	}
+
+	void Update(){
+	    DoPatrol();
+
+	    //Agentと目的地の距離 移動完了判定　or 敵の速度判定　update 
+	    //agentToPatroldistance = Vector3.Distance (this.agent.transform.position, pos);
+	    //Agentとプレイヤーの距離
+	    //agentToPlayerdistance = Vector3.Distance(this.agent.transform.position,PlayerPosition.transform.position);
+
+
+	    //プレイヤーとAgentの距離が10f以下になると追跡開始
+	    if(agentToPlayerdistance <= 10f ){
+	        DoTracking ();
+	    //プレイヤーと目的地の距離が15f以下になると次の目的地をランダム指定
+	    }else if(agentToPatroldistance < 15f){
+	        DoPatrol();
+	    }
+	}
+
+	    void OnTriggerEnter(Collider other) {  
+	        EnemyRotation();
+	    }  
+
+
+	    //エージェントが向かう先をランダムに指定するメソッド
+	    public void DoPatrol(){
+	        var x = Random.Range(-8.0f, 8.0f);
+	        var z = Random.Range(-8.0f, 8.0f);
+	        pos = new Vector3 (x, 0, z);
+	        bool targetpositionflug = agent.SetDestination (pos);
+	    }
+
+	    //playerを追いかけるメソッド
+	        public void  DoTracking(){
+	        pos = PlayerPosition.position; 
+	        agent.SetDestination (pos);
+	    }
+
+	    public void EnemyRotation() {
+	        transform.rotation = Quaternion.AngleAxis(30, Vector3.up);
+	    }  
+}
---
+++ Assets/UnityChan/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpead = 5f;
+	public float rotationspeed = 360f;
+
+	CharacterController characterController;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+		animator = GetComponentInChildren<Animator> ();
+
+		bool key = false;
+		if(Input.GetKeyDown(KeyCode.UpArrow)) key = true;
+		if(Input.GetKeyDown(KeyCode.DownArrow)) key = true;
+		if(Input.GetKeyDown(KeyCode.RightArrow)) key = true;
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) key = true;
+		if(key = true){
+			animator.SetTrigger("transration");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),0,
+			Input.GetAxis("Vertical"));
+			if (direction.sqrMagnitude > 0.01f) {
+				Vector3 forward = Vector3.Slerp(
+					transform.forward,
+					direction,
+					rotationspeed * Time.deltaTime / Vector3.Angle
+					(transform.forward, direction)
+				);
+				transform.LookAt(transform.position + forward);
+			}
+			characterController.Move(direction * moveSpead * Time.deltaTime);
+			
+		animator.SetFloat ("Speed", characterController.velocity.magnitude);
+
+		float Length;
+
+		if (GameObject.FindGameObjectsWithTag ("Dot").Length == 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+	//
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Dot") {
+			Destroy (other.gameObject);
+		}
+		if (other.tag == "Enemy"){
+			Application.LoadLevel ("Lose");
+		}
+	}
+}
